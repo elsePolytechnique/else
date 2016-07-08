1fc10ffa0d284efc5488fe77f704cd14f44db953
wilhelmhb <wilhelmhb@live.fr>
2016-07-08 18:21:10 +0200

    structure propre, en anglais, pour les membres

--- htdocs/member/models.py
+++ htdocs/member/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, _user_has_module_perms, _user_has_perm
-from django.db.models import CharField, EmailField, BooleanField, DateTimeField
+from django.db.models import CharField, EmailField, BooleanField, DateTimeField, TextField
 
 from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 from rest_framework.viewsets import ModelViewSet
@@ -11,23 +11,23 @@ from rest_framework import decorators
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
 
-class MembreManager(BaseUserManager):
+class MemberManager(BaseUserManager):
     def create_user(self, username, password):
-        membre = self.model(username=username)
-        membre.set_password(password)
-        membre.save(using=self._db)
-        return membre
+        member = self.model(username=username)
+        member.set_password(password)
+        member.save(using=self._db)
+        return member
 
     def create_superuser(self, username, password):
-        membre = self.create_user(username, password)
-        membre.is_superuser = True
-        membre.is_staff = True
-        membre.save(using=self._db)
-        return membre
+        member = self.create_user(username, password)
+        member.is_superuser = True
+        member.is_staff = True
+        member.save(using=self._db)
+        return member
 
-class Membre(AbstractBaseUser):
+class Member(AbstractBaseUser):
     class Meta:
-        app_label = 'membres'
+        app_label = 'member'
 
     #username = CharField(max_length=128, unique=True)
     email = EmailField(max_length=254, unique=True) # one user by email, no username
@@ -40,15 +40,6 @@ class Membre(AbstractBaseUser):
     firstname = CharField(max_length=128, blank=False) # First and last name are mandatory
     lastname = CharField(max_length=128, blank=False)
     # phone = # didn't know how to check validity
-    PROMOTIONS = [
-            ('X',2009,2015),
-            ('ENSTA',2013,2016),
-            ('IOGS',2013,2016),
-            ]
-    PROMOTIONS_CHOICES = [ ('XDoc','Doctorant') ]
-    for x in PROMOTIONS:
-        PROMOTIONS_CHOICES += [ (x[0]+str(i),x[0]+str(i)) for i in range(x[1],x[2]+1) ]
-    promotion = CharField(max_length=32, choices=PROMOTIONS_CHOICES, default='X2015',blank=False)
 
     GROUP_TYPES = [
             ('Bar','Bar'),
@@ -60,7 +51,7 @@ class Membre(AbstractBaseUser):
     group = CharField(max_length=128, blank=True)
     activity = TextField(max_length=256, blank=True)
 
-    is_active = BooleanField(default=True) # maybe default False for account validation ?
+    is_active = BooleanField(default=False) # maybe default False for account validation ?
     last_modified = DateTimeField(auto_now=True)
     created = DateTimeField(auto_now=True)
 
@@ -71,7 +62,7 @@ class Membre(AbstractBaseUser):
     previous_login = DateTimeField(blank=True, auto_now_add=True)
     current_login = DateTimeField(blank=True, auto_now_add=True)
 
-    objects = MembreManager()
+    objects = MemberManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -98,23 +89,23 @@ class Membre(AbstractBaseUser):
     def get_full_name(self):
         return "%s %s" % (self.firstname, self.lastname)
 
-class MembreSerializer(ModelSerializer):
+class MemberSerializer(ModelSerializer):
     class Meta:
-        model = Membre
+        model = Member
         read_only_fields = ('is_active', 'last_login', 'last_modified', 'previous_login', 'is_staff', 'is_superuser') #champs en lecture seule
         #fields = ('id', 'username') #soit tu choisis les champs à afficher
         exclude = () # soit tu choisis les champs à cacher
         extra_kwargs = {'password': { 'write_only': True, 'required':False }} #les champs spéciaux
 
     def create(self, data): #on met un mot de passe par défaut (si nécessaire)
-        u = super(MembreSerializer, self).create(data)
+        u = super(MemberSerializer, self).create(data)
         u.set_password(data.get('password', '0000'))
         u.save()
         return u
 
-class MembreViewSet(ModelViewSet): #ce qu'on verra dans la liste des membres
-    queryset = Membre.objects.filter(authorised=True) #l'ensemble des éléments à sélectionner, par exemple uniquement les membres dont la création de compte a été confirmée
-    serializer_class = MembreSerializer #le format des éléments que tu verras
+class MemberViewSet(ModelViewSet): #ce qu'on verra dans la liste des members
+    queryset = Member.objects.filter(is_active=True) #l'ensemble des éléments à sélectionner, par exemple uniquement les members dont la création de compte a été confirmée
+    serializer_class = MemberSerializer #le format des éléments que tu verras
     permission_classes = (IsAuthenticatedOrReadOnly,) #les permissions
 
 
@@ -128,28 +119,28 @@ class MembreViewSet(ModelViewSet): #ce qu'on verra dans la liste des membres
     @decorators.permission_classes((IsAuthenticated,))
     @decorators.list_route(methods=['put'])
     def change_password(self, request):
-        membre = request.user
+        member = request.user
 
         data = request.data
-        if not membre.check_password(data['old_password']):
+        if not member.check_password(data['old_password']):
             raise exceptions.PermissionDenied()
 
         if data['password'] == "":
             return Response("'password' field cannot be empty", status=HTTP_400_BAD_REQUEST)
 
-        membre.set_password(data['password'])
-        membre.save()
+        member.set_password(data['password'])
+        member.save()
         return Response('Password changed', status=HTTP_200_OK)
 
     @list_route(methods=['get'])
-    def liste_autocomplete(self, request):
-        membres = Membre.objects.filter(username__icontains=request.GET.get("recherche", "")).order_by('username')
-        serializer = MembreSerializer(membres, many=True)
+    def list_autocomplete(self, request):
+        members = Member.objects.filter(email__icontains=request.GET.get("search", "")).order_by('email')
+        serializer = MemberSerializer(members, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
     
     #là tu comprends pourquoi l'application admin n'est pas forcément utile...
     @list_route(methods=['get'])
-    def liste_admin(self, request):
-        membres = Membre.objects.all().order_by('authorised', 'username')
-        serializer = MembreSerializer(membres, many=True)
+    def list_admin(self, request):
+        members = Member.objects.all().order_by('is_active', 'username')
+        serializer = MemberSerializer(members, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
