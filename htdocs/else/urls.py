66749b300a6ad9c58629588ac5c2e40634934b5c
wilhelmhb <wilhelmhb@live.fr>
2016-07-08 18:24:28 +0200

    utilisation du router u rest framework pour voir le résultat

--- htdocs/else/urls.py
+++ htdocs/else/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework import routers
+from member.models import MemberViewSet
+
+router = routers.DefaultRouter()
+
+router.register('members', MemberViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
+    url(r'^', include(router.urls)),
 ]
